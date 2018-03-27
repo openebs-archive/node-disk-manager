dd45ffaf43817dcb4c4c1658e594f8a7698de27e
sagarkrsd <sagar.kumar@openebs.io>
2018-03-27 13:18:19 +0530

    Use a configurable base image in dockerfile
    Signed-off-by: sagarkrsd <sagar.kumar@openebs.io>

--- pkg/apis/ndm/v1alpha1/types.go
+++ pkg/apis/ndm/v1alpha1/types.go
@@ -13,17 +13,17 @@ import (
 // StoragePoolClaim describes a StoragePoolClaim.
 type StoragePoolClaim struct {
 	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata",omitempty`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	Spec StoragePoolClaimSpec `json:"spec"`
 }
 
 // StoragePoolClaimSpec is the spec for a StoragePoolClaimSpec resource
 type StoragePoolClaimSpec struct {
-	Name       string `json:"name"` // disk name (e.g. sdb, sdc)
-	Format     string `json:"format"` // filesystem format (e.g. ext3, ext4)
+	Name       string `json:"name"`       // disk name (e.g. sdb, sdc)
+	Format     string `json:"format"`     // filesystem format (e.g. ext3, ext4)
 	Mountpoint string `json:"mountpoint"` // where to mount the filesystem
-	Path       string `json:"path"` // disk path
+	Path       string `json:"path"`       // disk path
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -46,19 +46,19 @@ type StoragePoolClaimList struct {
 // StoragePool describes a StoragePool.
 type StoragePool struct {
 	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata",omitempty`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	Spec StoragePoolSpec `json:"spec"`
 }
 
 // StoragePoolSpec is the spec for a StoragePool resource
 type StoragePoolSpec struct {
-	Name       string `json:"name"` // name of the resource
-	Format     string `json:"format"` // filesystem format type (e.g. ext4, ext3)
+	Name       string `json:"name"`       // name of the resource
+	Format     string `json:"format"`     // filesystem format type (e.g. ext4, ext3)
 	Mountpoint string `json:"mountpoint"` // mount location
-	Nodename   string `json:"nodename"` // host name
-	Message    string `json:"message"` // description
-	Path       string `json:"path"` // disk path
+	Nodename   string `json:"nodename"`   // host name
+	Message    string `json:"message"`    // description
+	Path       string `json:"path"`       // disk path
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
