614b050bc70764c43880f65d2a8e345ab946fb7b
sagarkrsd <sagar.kumar@openebs.io>
2018-07-11 16:54:45 +0530

    Modularize build script and integrate shellcheck in ndm
    Signed-off-by: sagarkrsd <sagar.kumar@openebs.io>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,10 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ${GOPATH}/src/k8s.io/code-generator)}
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[*]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo "${GOPATH}"/src/k8s.io/code-generator)}
 
 vendor/k8s.io/code-generator/generate-groups.sh all \
   github.com/openebs/node-disk-manager/pkg/client github.com/openebs/node-disk-manager/pkg/apis \
   openebs.io:v1alpha1 \
-  --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
+  --go-header-file "${SCRIPT_ROOT}" /hack/custom-boilerplate.go.txt
