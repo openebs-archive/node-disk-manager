eea1af8c7e8abd83568ba66b73089453e6f6ffc5
Richard Burk Orofeo <10726631+rbo13@users.noreply.github.com>
2018-10-01 19:03:49 +0800

    Fixed go vet for github.com/openebs/node-disk-manager/pkg/apis/openebs.io/v1alpha1 (#127)

--- pkg/apis/openebs.io/v1alpha1/types.go
+++ pkg/apis/openebs.io/v1alpha1/types.go
@@ -13,7 +13,7 @@ import (
 // Disk describes disk resource.
 type Disk struct {
 	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata, omitempty"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	Spec   DiskSpec   `json:"spec"`
 	Status DiskStatus `json:"status"`
