33187b84bba644a55a0be915a9c9ce4038e1bb53
bege13mot <bege13mot@gmail.com>
2018-10-01 17:28:41 +0400

    Fix go vet errors in fdutil_test.go
    Signed-off-by: bege13mot <bege13mot@gmail.com>

--- pkg/util/fdutil_test.go
+++ pkg/util/fdutil_test.go
@@ -28,9 +28,9 @@ func TestFD_SET(t *testing.T) {
 		x        int
 		expected syscall.FdSet
 	}{
-		"fd set test for fd value 0": {x: 0, expected: syscall.FdSet{[16]int64{1}}},
-		"fd set test for fd value 1": {x: 1, expected: syscall.FdSet{[16]int64{2}}},
-		"fd set test for fd value 7": {x: 7, expected: syscall.FdSet{[16]int64{128}}},
+		"fd set test for fd value 0": {x: 0, expected: syscall.FdSet{[32]int32{1}}},
+		"fd set test for fd value 1": {x: 1, expected: syscall.FdSet{[32]int32{2}}},
+		"fd set test for fd value 7": {x: 7, expected: syscall.FdSet{[32]int32{128}}},
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
@@ -47,11 +47,11 @@ func TestFD_ISSET(t *testing.T) {
 		fdset    syscall.FdSet
 		expected bool
 	}{
-		"set 0 and check with 2^0": {x: 0, fdset: syscall.FdSet{[16]int64{1}}, expected: true},
-		"set 1 and check with 2^1": {x: 1, fdset: syscall.FdSet{[16]int64{2}}, expected: true},
-		"set 7 and check with 2^7": {x: 7, fdset: syscall.FdSet{[16]int64{128}}, expected: true},
-		"set 4 and check with 6":   {x: 4, fdset: syscall.FdSet{[16]int64{6}}, expected: false},
-		"set 2 and check with 2":   {x: 2, fdset: syscall.FdSet{[16]int64{2}}, expected: false},
+		"set 0 and check with 2^0": {x: 0, fdset: syscall.FdSet{[32]int32{1}}, expected: true},
+		"set 1 and check with 2^1": {x: 1, fdset: syscall.FdSet{[32]int32{2}}, expected: true},
+		"set 7 and check with 2^7": {x: 7, fdset: syscall.FdSet{[32]int32{128}}, expected: true},
+		"set 4 and check with 6":   {x: 4, fdset: syscall.FdSet{[32]int32{6}}, expected: false},
+		"set 2 and check with 2":   {x: 2, fdset: syscall.FdSet{[32]int32{2}}, expected: false},
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
@@ -66,9 +66,9 @@ func TestFD_ZERO(t *testing.T) {
 		x        int
 		expected syscall.FdSet
 	}{
-		"fd zero test for fdset value {[16]int64{1}}":   {x: 0, expected: syscall.FdSet{[16]int64{1}}},
-		"fd zero test for fdset value {[16]int64{2}}":   {x: 1, expected: syscall.FdSet{[16]int64{2}}},
-		"fd zero test for fdset value {[16]int64{128}}": {x: 7, expected: syscall.FdSet{[16]int64{128}}},
+		"fd zero test for fdset value {[16]int64{1}}":   {x: 0, expected: syscall.FdSet{[32]int32{1}}},
+		"fd zero test for fdset value {[16]int64{2}}":   {x: 1, expected: syscall.FdSet{[32]int32{2}}},
+		"fd zero test for fdset value {[16]int64{128}}": {x: 7, expected: syscall.FdSet{[32]int32{128}}},
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
