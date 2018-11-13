53fa2ce94b906141bf4aa691acfb5bda3e3dfea3
Akhil Mohan <akhilerm@gmail.com>
2018-11-13 10:09:18 +0400

    created separate contexts when path filter is enabled and disabled
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

--- integration_test/pathfilter_integration_test.go
+++ integration_test/pathfilter_integration_test.go
@@ -12,101 +12,105 @@ var _ = Describe("Path filter integration Test", func() {
 	var configMapPatch ndmutil.ConfigMapPatch
 	var err error
 
-	When("Path filter is disabled", func() {
-		err = ndmutil.InitEnvironment()
-		Expect(err).NotTo(HaveOccurred())
-
-		configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
-		configMapPatch.SetPathFilter("false")
-
-		ndmutil.ReplaceAndApplyConfig(configMapPatch)
-		// It waits till node-disk-manager is ready or timeout reached
-		err = ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
-		Expect(err).NotTo(HaveOccurred())
-
-		It("has matching disks inside node and the host", func() {
-
-			matched, err := ndmutil.MatchDisksOutsideAndInside()
-
+	Context("Path filter is disabled", func() {
+		When("We disable the path filter", func() {
+			err = ndmutil.InitEnvironment()
 			Expect(err).NotTo(HaveOccurred())
-			Expect(matched).To(BeTrue())
+
+			configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
+			configMapPatch.SetPathFilter("false")
+
+			ndmutil.ReplaceAndApplyConfig(configMapPatch)
+			// It waits till node-disk-manager is ready or timeout reached
+			err = ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
+			Expect(err).NotTo(HaveOccurred())
+
+			It("has matching disks inside node and the host", func() {
+
+				matched, err := ndmutil.MatchDisksOutsideAndInside()
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(matched).To(BeTrue())
+			})
 		})
 	})
 
-	When("A device is included", func() {
-		err = ndmutil.InitEnvironment()
-		Expect(err).NotTo(HaveOccurred())
+	Context("When path filter is enabled", func() {
 
-		configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
-		configMapPatch.SetIncludePath("/dev/sda")
-		ndmutil.ReplaceAndApplyConfig(configMapPatch)
-		// It waits till node-disk-manager is ready or timeout reached
-		err := ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
-		Expect(err).NotTo(HaveOccurred())
-
-		It("has only /dev/sda in the pod", func() {
-			matched, err := ndmutil.MatchNDMDeviceList(false, "/dev/sda")
-
+		When("We include a single device", func() {
+			err = ndmutil.InitEnvironment()
 			Expect(err).NotTo(HaveOccurred())
-			Expect(matched).To(BeTrue())
+
+			configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
+			configMapPatch.SetIncludePath("/dev/sda")
+			ndmutil.ReplaceAndApplyConfig(configMapPatch)
+			// It waits till node-disk-manager is ready or timeout reached
+			err := ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
+			Expect(err).NotTo(HaveOccurred())
+
+			It("has only /dev/sda in the pod", func() {
+				matched, err := ndmutil.MatchNDMDeviceList(false, "/dev/sda")
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(matched).To(BeTrue())
+			})
+		})
+
+		When("We exclude a single device", func() {
+			err = ndmutil.InitEnvironment()
+			Expect(err).NotTo(HaveOccurred())
+
+			configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
+			configMapPatch.SetExcludePath("/dev/sda")
+			ndmutil.ReplaceAndApplyConfig(configMapPatch)
+			// It waits till node-disk-manager is ready or timeout reached
+			err := ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
+			Expect(err).NotTo(HaveOccurred())
+
+			It("does not have /dev/sda in the pod", func() {
+				matched, err := ndmutil.MatchNDMDeviceList(true, "/dev/sda")
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(matched).To(BeTrue())
+			})
+		})
+
+		When("We include 2 devices", func() {
+			err = ndmutil.InitEnvironment()
+			Expect(err).NotTo(HaveOccurred())
+
+			configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
+			configMapPatch.SetIncludePath("/dev/sda", "/dev/vda")
+			ndmutil.ReplaceAndApplyConfig(configMapPatch)
+			// It waits till node-disk-manager is ready or timeout reached
+			err := ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
+			Expect(err).NotTo(HaveOccurred())
+
+			It("have both `/dev/sda` and `/dev/vda` in the pod", func() {
+				matched, err := ndmutil.MatchNDMDeviceList(false, "/dev/sda", "/dev/vda")
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(matched).To(BeTrue())
+			})
+		})
+
+		When("We exclude 2 devices", func() {
+			err = ndmutil.InitEnvironment()
+			Expect(err).NotTo(HaveOccurred())
+
+			configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
+			configMapPatch.SetExcludePath("/dev/sda", "/dev/vda")
+			ndmutil.ReplaceAndApplyConfig(configMapPatch)
+			// It waits till node-disk-manager is ready or timeout reached
+			err := ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
+			Expect(err).NotTo(HaveOccurred())
+
+			It("doesn't have either `/dev/sda` or `/dev/vda` in the pod", func() {
+				matched, err := ndmutil.MatchNDMDeviceList(true, "/dev/sda", "/dev/vda")
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(matched).To(BeTrue())
+			})
 		})
 	})
-
-	When("A device is excluded", func() {
-		err = ndmutil.InitEnvironment()
-		Expect(err).NotTo(HaveOccurred())
-
-		configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
-		configMapPatch.SetExcludePath("/dev/sda")
-		ndmutil.ReplaceAndApplyConfig(configMapPatch)
-		// It waits till node-disk-manager is ready or timeout reached
-		err := ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
-		Expect(err).NotTo(HaveOccurred())
-
-		It("does not have /dev/sda in the pod", func() {
-			matched, err := ndmutil.MatchNDMDeviceList(true, "/dev/sda")
-
-			Expect(err).NotTo(HaveOccurred())
-			Expect(matched).To(BeTrue())
-		})
-	})
-
-	When(" 2 devices are included ", func() {
-		err = ndmutil.InitEnvironment()
-		Expect(err).NotTo(HaveOccurred())
-
-		configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
-		configMapPatch.SetIncludePath("/dev/sda", "/dev/vda")
-		ndmutil.ReplaceAndApplyConfig(configMapPatch)
-		// It waits till node-disk-manager is ready or timeout reached
-		err := ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
-		Expect(err).NotTo(HaveOccurred())
-
-		It("have both `/dev/sda` and `/dev/vda` in the pod", func() {
-			matched, err := ndmutil.MatchNDMDeviceList(false, "/dev/sda", "/dev/vda")
-
-			Expect(err).NotTo(HaveOccurred())
-			Expect(matched).To(BeTrue())
-		})
-	})
-
-	When(" 2 devices are excluded ", func() {
-		err = ndmutil.InitEnvironment()
-		Expect(err).NotTo(HaveOccurred())
-
-		configMapPatch = ndmutil.GetNDMConfig(ndmutil.GetNDMOperatorFilePath())
-		configMapPatch.SetExcludePath("/dev/sda", "/dev/vda")
-		ndmutil.ReplaceAndApplyConfig(configMapPatch)
-		// It waits till node-disk-manager is ready or timeout reached
-		err := ndmutil.WaitTillNDMisUpOrTimeout(5 * time.Minute)
-		Expect(err).NotTo(HaveOccurred())
-
-		It("doesn't have either `/dev/sda` or `/dev/vda` in the pod", func() {
-			matched, err := ndmutil.MatchNDMDeviceList(true, "/dev/sda", "/dev/vda")
-
-			Expect(err).NotTo(HaveOccurred())
-			Expect(matched).To(BeTrue())
-		})
-	})
-
 })
