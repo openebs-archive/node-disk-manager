66ebae0805aab3b9d0008030b2822e413c810f38
satbir <satbir.singh@mayadata.io>
2019-03-22 10:04:07 +0530

    [US4449] feat(NDM-Pod Liveness check): initial commit for liveness check controller.
    Liveness check implemented using operator-sdk controller. New custom
    resource (ndmconfig) getting introduced as part of feature. This commit
    is to push auto-generated code for new controller & ndmconfig CRD.
    
    Signed-off-by: satbir <satbir.singh@mayadata.io>

---
+++ pkg/apis/openebs/v1alpha1/ndmconfig_types.go
@@ -0,0 +1,45 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NdmConfigSpec defines the desired state of NdmConfig
+type NdmConfigSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// NdmConfigStatus defines the observed state of NdmConfig
+type NdmConfigStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NdmConfig is the Schema for the ndmconfigs API
+// +k8s:openapi-gen=true
+type NdmConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NdmConfigSpec   `json:"spec,omitempty"`
+	Status NdmConfigStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NdmConfigList contains a list of NdmConfig
+type NdmConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NdmConfig `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NdmConfig{}, &NdmConfigList{})
+}
