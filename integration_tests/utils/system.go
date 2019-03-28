da75999c53bd4543fdc401c838eaac0bcd5f0d6f
Akhil Mohan <akhil.mohan@mayadata.io>
2019-03-28 14:45:23 +0530

    fixed error message in system-utils
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- integration_tests/utils/system.go
+++ integration_tests/utils/system.go
@@ -25,6 +25,9 @@ func RunCommand(cmd string) error {
 	name := substring[0]
 	args := substring[1:]
 	err := exec.Command(name, args...).Run()
+	if err != nil {
+		return fmt.Errorf("run failed %s %v", cmd, err)
+	}
 	return err
 }
 
@@ -34,6 +37,9 @@ func ExecCommand(cmd string) (string, error) {
 	name := substring[0]
 	args := substring[1:]
 	out, err := exec.Command(name, args...).CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("exec failed %s %v", cmd, err)
+	}
 	return string(out), err
 }
 
