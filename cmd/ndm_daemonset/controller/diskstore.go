dd31e0024a44c685b0f613701f5cdc03ca765090
Akhil Mohan <akhil.mohan@mayadata.io>
2019-05-14 16:10:42 +0530

    refactor(sparse-disk): removed deactivating sparse disk
    Only sparse blockdevice will be made available. Therefore, removing
    stale disk resources will not take care of sparse disks.
    
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- cmd/ndm_daemonset/controller/diskstore.go
+++ cmd/ndm_daemonset/controller/diskstore.go
@@ -162,14 +162,13 @@ func (c *Controller) GetExistingDiskResource(listDr *apis.DiskList, uuid string)
 // list of active resources. One active resource which is present in etcd not in
 // system that will be marked as inactive.
 func (c *Controller) DeactivateStaleDiskResource(devices []string) {
-	listDisks := append(devices, GetActiveSparseDisksUuids(c.HostName)...)
 	listDR, err := c.ListDiskResource()
 	if err != nil {
 		glog.Error(err)
 		return
 	}
 	for _, item := range listDR.Items {
-		if !util.Contains(listDisks, item.ObjectMeta.Name) {
+		if !util.Contains(devices, item.ObjectMeta.Name) {
 			c.DeactivateDisk(item)
 		}
 	}
