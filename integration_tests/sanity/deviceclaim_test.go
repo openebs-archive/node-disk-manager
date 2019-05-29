ed03acddf5d8d5ad3b0bf1cb4c4c1f2d2d7fd5f8
Akhil Mohan <akhil.mohan@mayadata.io>
2019-05-29 20:02:45 +0530

    feat(BDD): added block device claim integration tests
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

---
+++ integration_tests/sanity/deviceclaim_test.go
@@ -0,0 +1,180 @@
+package sanity
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/openebs/node-disk-manager/integration_tests/k8s"
+	"github.com/openebs/node-disk-manager/integration_tests/udev"
+	apis "github.com/openebs/node-disk-manager/pkg/apis/openebs/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+var (
+	BDCUnavailableCapacity = resource.MustParse("10Gi")
+	BDCAvailableCapacity   = resource.MustParse("1Gi")
+	BDCInvalidCapacity     = resource.MustParse("0")
+)
+
+var _ = Describe("BlockDevice Claim tests", func() {
+
+	// attach a new physical disk
+	physicalDisk := udev.NewDisk(DiskImageSize)
+	_ = physicalDisk.AttachDisk()
+
+	k8sClient, _ := k8s.GetClientSet()
+
+	BeforeEach(func() {
+		err := k8sClient.CreateNDMYAML()
+		Expect(err).NotTo(HaveOccurred())
+		k8s.WaitForStateChange()
+		k8sClient, _ = k8s.GetClientSet()
+		//k8sClient.WaitForBDC()
+	})
+	AfterEach(func() {
+		err := k8sClient.DeleteNDMYAML()
+		Expect(err).NotTo(HaveOccurred())
+		k8s.WaitForStateChange()
+	})
+
+	Context("Claim Block Device when matching BD is not available", func() {
+		bdcName := "test-blockdeviceclaim"
+		var blockDeviceClaim *apis.BlockDeviceClaim
+		BeforeEach(func() {
+			blockDeviceClaim = k8s.NewBDC(bdcName)
+		})
+		AfterEach(func() {
+			err := k8sClient.DeleteBlockDeviceClaim(blockDeviceClaim)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("BD is not available on the host", func() {
+			blockDeviceClaim.Spec.HostName = "fake-minikube"
+			blockDeviceClaim.Spec.Requirements.Requests[apis.ResourceCapacity] = BDCAvailableCapacity
+
+			err := k8sClient.CreateBlockDeviceClaim(blockDeviceClaim)
+			Expect(err).NotTo(HaveOccurred())
+			k8s.WaitForReconcilation()
+
+			bdcList, err := k8sClient.ListBlockDeviceClaims()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(bdcList.Items)).NotTo(BeZero())
+
+			for _, bdc := range bdcList.Items {
+				if bdc.Name == bdcName {
+					Expect(bdc.Status.Phase).To(Equal(apis.BlockDeviceClaimStatusEmpty))
+				}
+			}
+		})
+		It("BD with resource requirement is not available on the host", func() {
+			blockDeviceClaim.Spec.HostName = "minikube"
+			blockDeviceClaim.Spec.Requirements.Requests[apis.ResourceCapacity] = BDCUnavailableCapacity
+
+			err := k8sClient.CreateBlockDeviceClaim(blockDeviceClaim)
+			Expect(err).NotTo(HaveOccurred())
+			k8s.WaitForReconcilation()
+
+			bdcList, err := k8sClient.ListBlockDeviceClaims()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(bdcList.Items)).NotTo(BeZero())
+
+			for _, bdc := range bdcList.Items {
+				if bdc.Name == bdcName {
+					Expect(bdc.Status.Phase).To(Equal(apis.BlockDeviceClaimStatusPending))
+				}
+			}
+		})
+	})
+
+	Context("Claim Block Device when matching BD is available", func() {
+		bdcName := "test-blockdeviceclaim"
+		var blockDeviceClaim *apis.BlockDeviceClaim
+		BeforeEach(func() {
+			blockDeviceClaim = k8s.NewBDC(bdcName)
+		})
+		AfterEach(func() {
+			err := k8sClient.DeleteBlockDeviceClaim(blockDeviceClaim)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("has matching BD on the node", func() {
+			blockDeviceClaim.Spec.HostName = "minikube"
+			blockDeviceClaim.Spec.Requirements.Requests[apis.ResourceCapacity] = BDCAvailableCapacity
+
+			err := k8sClient.CreateBlockDeviceClaim(blockDeviceClaim)
+			Expect(err).NotTo(HaveOccurred())
+			k8s.WaitForReconcilation()
+
+			bdcList, err := k8sClient.ListBlockDeviceClaims()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(bdcList.Items)).NotTo(BeZero())
+
+			var bdName string
+			// check status of BDC
+			for _, bdc := range bdcList.Items {
+				if bdc.Name == bdcName {
+					bdName = bdc.Spec.BlockDeviceName
+					Expect(bdc.Status.Phase).To(Equal(apis.BlockDeviceClaimStatusDone))
+				}
+			}
+
+			// check status of BD that has been bound
+			bdList, err := k8sClient.ListBlockDevices()
+			Expect(err).NotTo(HaveOccurred())
+
+			for _, bd := range bdList.Items {
+				if bd.Name == bdName {
+					Expect(string(bd.Status.ClaimState)).To(Equal(apis.BlockDeviceClaimed))
+				}
+			}
+
+		})
+	})
+	Context("Unclamining a block device ", func() {
+		It("unclaimes a BD when BDC is deleted", func() {
+			bdcName := "test-blockdeviceclaim"
+			blockDeviceClaim := k8s.NewBDC(bdcName)
+			blockDeviceClaim.Spec.HostName = "minikube"
+			blockDeviceClaim.Spec.Requirements.Requests[apis.ResourceCapacity] = BDCAvailableCapacity
+			err := k8sClient.CreateBlockDeviceClaim(blockDeviceClaim)
+			Expect(err).NotTo(HaveOccurred())
+			k8s.WaitForReconcilation()
+
+			bdcList, err := k8sClient.ListBlockDeviceClaims()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(bdcList.Items)).NotTo(BeZero())
+
+			var bdName string
+			// check status of BDC
+			for _, bdc := range bdcList.Items {
+				if bdc.Name == bdcName {
+					bdName = bdc.Spec.BlockDeviceName
+					Expect(bdc.Status.Phase).To(Equal(apis.BlockDeviceClaimStatusDone))
+				}
+			}
+
+			// check status of BD that has been bound
+			bdList, err := k8sClient.ListBlockDevices()
+			Expect(err).NotTo(HaveOccurred())
+
+			for _, bd := range bdList.Items {
+				if bd.Name == bdName {
+					Expect(string(bd.Status.ClaimState)).To(Equal(apis.BlockDeviceClaimed))
+				}
+			}
+
+			err = k8sClient.DeleteBlockDeviceClaim(blockDeviceClaim)
+			Expect(err).NotTo(HaveOccurred())
+			k8s.WaitForReconcilation()
+
+			// check status of BD again to check it has been unclaimed
+			bdList, err = k8sClient.ListBlockDevices()
+			Expect(err).NotTo(HaveOccurred())
+			for _, bd := range bdList.Items {
+				if bd.Name == bdName {
+					Expect(string(bd.Status.ClaimState)).To(Equal(apis.BlockDeviceUnclaimed))
+				}
+			}
+
+		})
+	})
+
+})
