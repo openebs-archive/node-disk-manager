72aab644ae843332a89d0c928af67a4f85c54149
Akhil Mohan <akhil.mohan@mayadata.io>
2019-06-12 19:50:57 +0530

    fix(comments): add comments to methods.
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- pkg/cleaner/cleaner.go
+++ pkg/cleaner/cleaner.go
@@ -30,10 +30,13 @@ type Cleaner struct {
 	CleanupStatus *CleanupStatusTracker
 }
 
+// CleanupStatusTracker is used to track the cleanup state using
+// info provided by JobController
 type CleanupStatusTracker struct {
 	JobController JobController
 }
 
+// NewCleaner creates a new cleaner which can be used for cleaning BD, and checking status of the job
 func NewCleaner(client client.Client, namespace string, cleanupTracker *CleanupStatusTracker) *Cleaner {
 	return &Cleaner{
 		Client:        client,
@@ -42,6 +45,9 @@ func NewCleaner(client client.Client, namespace string, cleanupTracker *CleanupS
 	}
 }
 
+// Clean will launch a job to delete data on the BD depending on the
+// volume mode. Job will be launched only if another job is not running or a
+// job is in unknown state
 func (c *Cleaner) Clean(blockDevice *v1alpha1.BlockDevice) (bool, error) {
 	bdName := blockDevice.Name
 	if c.CleanupStatus.InProgress(bdName) {
@@ -65,14 +71,19 @@ func (c *Cleaner) Clean(blockDevice *v1alpha1.BlockDevice) (bool, error) {
 	return false, err
 }
 
+// InProgress returns whether a cleanup job is currently being done
+// for the given BD
 func (c *CleanupStatusTracker) InProgress(bdName string) bool {
 	return c.JobController.IsCleaningJobRunning(bdName)
 }
 
+// RemoveStatus returns the Cleanupstate of a job. If job is succeeded, it will
+// be deleted
 func (c *CleanupStatusTracker) RemoveStatus(bdName string) (CleanupState, error) {
 	return c.JobController.RemoveJob(bdName)
 }
 
+// runJob creates a new cleanup job in the namespace
 func (c *Cleaner) runJob(bd *v1alpha1.BlockDevice, volumeMode VolumeMode) error {
 	job, err := NewCleanupJob(bd, volumeMode, c.Namespace)
 	if err != nil {
@@ -81,6 +92,7 @@ func (c *Cleaner) runJob(bd *v1alpha1.BlockDevice, volumeMode VolumeMode) error 
 	return c.Client.Create(context.TODO(), job)
 }
 
+// getVolumeMode returns the volume mode of the BD that is being released
 func getVolumeMode(spec v1alpha1.DeviceSpec) VolumeMode {
 	if spec.FileSystem.Mountpoint != "" {
 		return VolumeModeFileSystem
