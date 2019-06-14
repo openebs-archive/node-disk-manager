5a363c1c97cda6a300d8e6214dd3f2fe63eee4e9
Akhil Mohan <akhil.mohan@mayadata.io>
2019-06-14 14:59:57 +0530

    fix(BDD): change type of claim state from string to DeviceClaimState
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- integration_tests/sanity/deviceclaim_test.go
+++ integration_tests/sanity/deviceclaim_test.go
@@ -134,7 +134,7 @@ var _ = Describe("BlockDevice Claim tests", func() {
 
 			for _, bd := range bdList.Items {
 				if bd.Name == bdName {
-					Expect(string(bd.Status.ClaimState)).To(Equal(apis.BlockDeviceClaimed))
+					Expect(bd.Status.ClaimState).To(Equal(apis.BlockDeviceClaimed))
 				}
 			}
 
@@ -170,7 +170,7 @@ var _ = Describe("BlockDevice Claim tests", func() {
 
 			for _, bd := range bdList.Items {
 				if bd.Name == bdName {
-					Expect(string(bd.Status.ClaimState)).To(Equal(apis.BlockDeviceClaimed))
+					Expect(bd.Status.ClaimState).To(Equal(apis.BlockDeviceClaimed))
 				}
 			}
 
@@ -183,7 +183,7 @@ var _ = Describe("BlockDevice Claim tests", func() {
 			Expect(err).NotTo(HaveOccurred())
 			for _, bd := range bdList.Items {
 				if bd.Name == bdName {
-					Expect(string(bd.Status.ClaimState)).To(Equal(apis.BlockDeviceReleased))
+					Expect(bd.Status.ClaimState).To(Equal(apis.BlockDeviceUnclaimed))
 				}
 			}
 
