7124f823353ffa59f8ce8b0adfb6aca10680cbb0
Akhil Mohan <akhil.mohan@mayadata.io>
2019-07-04 18:43:50 +0530

    fix(install): fix issues reported by Codacy
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- pkg/apis/openebs/v1alpha1/crds.go
+++ pkg/apis/openebs/v1alpha1/crds.go
@@ -6,26 +6,38 @@ import (
 )
 
 const (
-	// Disk Resource
-	DiskResourceKind     = "Disk"
+	// DiskResourceKind is the kind of Disk CRD
+	DiskResourceKind = "Disk"
+	// DiskResourceListKind is the list kind for Disk
 	DiskResourceListKind = "DiskList"
-	DiskResourcePlural   = "disks"
-	DiskResourceShort    = "disk"
-	DiskResourceName     = DiskResourcePlural + "." + GroupName
+	// DiskResourcePlural is the plural form used for disk
+	DiskResourcePlural = "disks"
+	// DiskResourceShort is the short name used for disk CRD
+	DiskResourceShort = "disk"
+	// DiskResourceName is the name of the disk resource
+	DiskResourceName = DiskResourcePlural + "." + GroupName
 
-	// BlockDevice Resource
-	BlockDeviceResourceKind     = "BlockDevice"
+	// BlockDeviceResourceKind is the kind of block device CRD
+	BlockDeviceResourceKind = "BlockDevice"
+	// BlockDeviceResourceListKind is the list kind for block device
 	BlockDeviceResourceListKind = "BlockDeviceList"
-	BlockDeviceResourcePlural   = "blockdevices"
-	BlockDeviceResourceShort    = "bd"
-	BlockDeviceResourceName     = BlockDeviceResourcePlural + "." + GroupName
+	// BlockDeviceResourcePlural is the plural form used for block device
+	BlockDeviceResourcePlural = "blockdevices"
+	// BlockDeviceResourceShort is the short name used for block device CRD
+	BlockDeviceResourceShort = "bd"
+	// BlockDeviceResourceName is the name of the block device resource
+	BlockDeviceResourceName = BlockDeviceResourcePlural + "." + GroupName
 
-	// BlockDevice Resource
-	BlockDeviceClaimResourceKind     = "BlockDeviceClaim"
+	// BlockDeviceClaimResourceKind is the kind of block device claim CRD
+	BlockDeviceClaimResourceKind = "BlockDeviceClaim"
+	// BlockDeviceClaimResourceListKind is the list kind for block device claim
 	BlockDeviceClaimResourceListKind = "BlockDeviceClaimList"
-	BlockDeviceClaimResourcePlural   = "blockdeviceclaims"
-	BlockDeviceClaimResourceShort    = "bdc"
-	BlockDeviceClaimResourceName     = BlockDeviceClaimResourcePlural + "." + GroupName
+	// BlockDeviceClaimResourcePlural is the plural form used for block device claim
+	BlockDeviceClaimResourcePlural = "blockdeviceclaims"
+	// BlockDeviceClaimResourceShort is the short name used for block device claim CRD
+	BlockDeviceClaimResourceShort = "bdc"
+	// BlockDeviceClaimResourceName is the name of the block device claim resource
+	BlockDeviceClaimResourceName = BlockDeviceClaimResourcePlural + "." + GroupName
 )
 
 func buildCRD(name, kind, listKind, plural, short string, scope apiext.ResourceScope) *apiext.CustomResourceDefinition {
