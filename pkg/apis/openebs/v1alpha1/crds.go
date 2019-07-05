373e0f002f7927d0ec09def11bdec11fdc98664d
Akhil Mohan <akhil.mohan@mayadata.io>
2019-07-05 17:41:12 +0530

    refact(apis): remove CRD build code from apis
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- pkg/apis/openebs/v1alpha1/crds.go
+++ pkg/apis/openebs/v1alpha1/crds.go
@@ -1,10 +1,5 @@
 package v1alpha1
 
-import (
-	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
 const (
 	// DiskResourceKind is the kind of Disk CRD
 	DiskResourceKind = "Disk"
@@ -39,52 +34,3 @@ const (
 	// BlockDeviceClaimResourceName is the name of the block device claim resource
 	BlockDeviceClaimResourceName = BlockDeviceClaimResourcePlural + "." + GroupName
 )
-
-func buildCRD(name, kind, listKind, plural, short string, scope apiext.ResourceScope) *apiext.CustomResourceDefinition {
-	return &apiext.CustomResourceDefinition{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Spec: apiext.CustomResourceDefinitionSpec{
-			Group:   SchemeGroupVersion.Group,
-			Version: SchemeGroupVersion.Version,
-			Scope:   scope,
-			Names: apiext.CustomResourceDefinitionNames{
-				Kind:       kind,
-				ListKind:   listKind,
-				Plural:     plural,
-				ShortNames: []string{short},
-			},
-		},
-	}
-}
-
-// DiskCRD returns a cluster-scoped disk CustomResourceDefinition
-func DiskCRD() *apiext.CustomResourceDefinition {
-	return buildCRD(DiskResourceName,
-		DiskResourceKind,
-		DiskResourceListKind,
-		DiskResourcePlural,
-		DiskResourceShort,
-		apiext.ClusterScoped)
-}
-
-// BlockDeviceCRD returns a namespace-scoped blockdevice CustomResourceDefinition
-func BlockDeviceCRD() *apiext.CustomResourceDefinition {
-	return buildCRD(BlockDeviceResourceName,
-		BlockDeviceResourceKind,
-		BlockDeviceResourceListKind,
-		BlockDeviceResourcePlural,
-		BlockDeviceResourceShort,
-		apiext.NamespaceScoped)
-}
-
-// BlockDeviceClaimCRD returns a namespace scoped blockdeviceclaim CustomResourceDefinition
-func BlockDeviceClaimCRD() *apiext.CustomResourceDefinition {
-	return buildCRD(BlockDeviceClaimResourceName,
-		BlockDeviceClaimResourceKind,
-		BlockDeviceClaimResourceListKind,
-		BlockDeviceClaimResourcePlural,
-		BlockDeviceClaimResourceShort,
-		apiext.NamespaceScoped)
-}
