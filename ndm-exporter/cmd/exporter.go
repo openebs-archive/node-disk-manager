9a0df14830192f7daec4b2366ba9f71784f08ad4
Akhil Mohan <akhil.mohan@mayadata.io>
2019-09-16 18:44:36 +0530

    add pkg for ndm-exporter
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

---
+++ ndm-exporter/cmd/exporter.go
@@ -0,0 +1,104 @@
+package cmd
+
+import (
+	"github.com/golang/glog"
+	"github.com/openebs/node-disk-manager/ndm-exporter/collector/static"
+	"github.com/openebs/node-disk-manager/pkg/apis"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+type Exporter struct {
+	Client client.Client
+	ExporterOptions
+}
+
+const (
+	ClusterLevel = "cluster"
+	NodeLevel    = "node"
+	Port         = ":8080"
+	MetricsPath  = "/metrics"
+)
+
+type ExporterOptions struct {
+	// Mode to run the exporter. Can be either cluster or node
+	Mode string
+
+	// Port to listen
+	Port string
+
+	// MetricsPath to query for the metrics
+	MetricsPath string
+}
+
+func (e *Exporter) Run() error {
+	var err error
+	switch e.Mode {
+	case ClusterLevel:
+		err = runClusterExporter()
+	case NodeLevel:
+		err = runNodeExporter()
+	}
+
+	if err != nil {
+		glog.Error("error in running exporter")
+		return err
+	}
+
+	// starting prometheus http handler
+	http.Handle(e.MetricsPath, promhttp.Handler())
+	err = http.ListenAndServe(e.Port, nil)
+	if err != nil {
+		glog.Errorf("error in starting http server. %v", err)
+		return err
+	}
+	glog.Info("Server started on %s at %s endpoint", e.Port, e.MetricsPath)
+	return nil
+}
+
+func runClusterExporter() error {
+	glog.Info("Starting cluster level exporter . . .")
+
+	// get the kube config
+	cfg, err := config.GetConfig()
+	if err != nil {
+		glog.Errorf("error getting config", err)
+		return err
+	}
+
+	// generate the client
+	k8sClient, err := client.New(cfg, client.Options{})
+	if err != nil {
+		glog.Errorf("error creating k8s client", err)
+		return err
+	}
+
+	// generate a manager. This is required for registering the APIs
+	mgr, err := manager.New(cfg, manager.Options{})
+	if err != nil {
+		glog.Errorf("error creating manager", err)
+		return err
+	}
+
+	// Setup Scheme for all resources
+	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
+		glog.Errorf("error registering apis", err)
+		return err
+	}
+
+	// create instance of a new static collector and register it.
+	c := static.New(k8sClient)
+	prometheus.MustRegister(c)
+
+	return nil
+}
+
+func runNodeExporter() error {
+	glog.Info("Starting node level exporter . . .")
+	// TODO code for node exporter
+	return nil
+}
---
+++ ndm-exporter/collector/static/static.go
@@ -0,0 +1,108 @@
+package static
+
+import (
+	"context"
+	"github.com/golang/glog"
+	"github.com/openebs/node-disk-manager/cmd/ndm_daemonset/controller"
+	"github.com/openebs/node-disk-manager/pkg/apis/openebs/v1alpha1"
+	"github.com/prometheus/client_golang/prometheus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sync"
+)
+
+type MetricCollector struct {
+	// Client is the k8s client which will be used to interface with etcd
+	Client client.Client
+
+	// concurrency handling
+	sync.Mutex
+	requestInProgress bool
+
+	// all the exposed metrics
+	*metrics
+}
+
+func New(client client.Client) prometheus.Collector {
+	return &MetricCollector{
+		Client: client,
+		metrics: newMetrics().
+			withBlockDeviceState().
+			withRejectRequest(),
+	}
+}
+
+func (mc *MetricCollector) setRequestProgressToFalse() {
+	mc.Lock()
+	mc.requestInProgress = false
+	mc.Unlock()
+}
+
+func (mc *MetricCollector) Describe(ch chan<- *prometheus.Desc) {
+	for _, col := range mc.collectors() {
+		col.Describe(ch)
+	}
+}
+
+func (mc *MetricCollector) collectors() []prometheus.Collector {
+	return []prometheus.Collector{
+		mc.blockDeviceState,
+		mc.rejectRequestCount,
+	}
+}
+
+func (mc *MetricCollector) Collect(ch chan<- prometheus.Metric) {
+
+	// when a second request comes, and the first one is already in progress,
+	// ignore/reject the second request
+	mc.Lock()
+	if mc.requestInProgress {
+		mc.rejectRequestCount.Inc()
+		mc.Unlock()
+		mc.rejectRequestCount.Collect(ch)
+		return
+	}
+
+	mc.requestInProgress = true
+	mc.Unlock()
+
+	// once a request is processed, set the progress flag to false
+	defer mc.setRequestProgressToFalse()
+
+	// get required metric data from etcd
+	bdList, err := mc.getMetricData()
+	if err != nil {
+		return
+	}
+
+	// set the metric data into the respective fields
+	mc.setMetricData(bdList)
+
+	// collect each metric
+	for _, col := range mc.collectors() {
+		col.Collect(ch)
+	}
+}
+
+// getMetricData is used to get the metric data from the source
+func (mc *MetricCollector) getMetricData() ([]v1alpha1.BlockDevice, error) {
+	bdlist := &v1alpha1.BlockDeviceList{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "BlockDevice",
+			APIVersion: "openebs.io/v1alpha1",
+		},
+	}
+	err := mc.Client.List(context.TODO(), nil, bdlist)
+	if err != nil {
+		glog.Error("error in listing BDs", err)
+		return nil, err
+	}
+	return bdlist.Items, nil
+}
+
+// setMetricData sets the metric data into respective fields
+func (mc *MetricCollector) setMetricData(bdList []v1alpha1.BlockDevice) {
+	for _, bd := range bdList {
+		mc.blockDeviceState.WithLabelValues(bd.Name, bd.Spec.Path, bd.Labels[controller.KubernetesHostNameLabel]).Set(getState(bd.Status.State))
+	}
+}
