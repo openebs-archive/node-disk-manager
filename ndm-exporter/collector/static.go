b62565c9b1bff912450bc4443d036eda99de7e0e
Akhil Mohan <akhil.mohan@mayadata.io>
2019-09-17 14:49:33 +0530

    metrics and collectors are now separated
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- ndm-exporter/collector/static.go
+++ ndm-exporter/collector/static.go
@@ -14,22 +14,24 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package static
+package collector
 
 import (
 	"context"
 	"github.com/golang/glog"
+	"github.com/openebs/node-disk-manager/blockdevice"
 	"github.com/openebs/node-disk-manager/cmd/ndm_daemonset/controller"
-	"github.com/openebs/node-disk-manager/pkg/apis/openebs/v1alpha1"
+	apis "github.com/openebs/node-disk-manager/pkg/apis/openebs/v1alpha1"
+	"github.com/openebs/node-disk-manager/pkg/metrics/static"
 	"github.com/prometheus/client_golang/prometheus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sync"
 )
 
-// MetricCollector contains the metrics, concurrency handler and client to get the
+// StaticMetricCollector contains the metrics, concurrency handler and client to get the
 // static metrics
-type MetricCollector struct {
+type StaticMetricCollector struct {
 	// Client is the k8s client which will be used to interface with etcd
 	Client client.Client
 
@@ -38,53 +40,45 @@ type MetricCollector struct {
 	requestInProgress bool
 
 	// all the exposed metrics
-	*metrics
+	metrics *static.Metrics
 }
 
-// New creates a new instance of MetricCollector which
+// New creates a new instance of StaticMetricCollector which
 // implements Collector interface
 func New(client client.Client) prometheus.Collector {
-	return &MetricCollector{
-		Client: client,
-		metrics: newMetrics().
-			withBlockDeviceState().
-			withRejectRequest(),
+	return &StaticMetricCollector{
+		Client:  client,
+		metrics: static.NewMetrics(),
 	}
 }
 
 // setRequestProgressToFalse is used to set the progress flag, when a request is
 // processed or errored
-func (mc *MetricCollector) setRequestProgressToFalse() {
+func (mc *StaticMetricCollector) setRequestProgressToFalse() {
 	mc.Lock()
 	mc.requestInProgress = false
 	mc.Unlock()
 }
 
 // Describe is the implementation of Describe in prometheus.Collector
-func (mc *MetricCollector) Describe(ch chan<- *prometheus.Desc) {
-	for _, col := range mc.collectors() {
+func (mc *StaticMetricCollector) Describe(ch chan<- *prometheus.Desc) {
+	for _, col := range mc.metrics.Collectors() {
 		col.Describe(ch)
 	}
 }
 
-// collectors lists out all the collectors for which the metrics is exposed
-func (mc *MetricCollector) collectors() []prometheus.Collector {
-	return []prometheus.Collector{
-		mc.blockDeviceState,
-		mc.rejectRequestCount,
-	}
-}
-
 // Collect is the implementation of Collect in prometheus.Collector
-func (mc *MetricCollector) Collect(ch chan<- prometheus.Metric) {
+func (mc *StaticMetricCollector) Collect(ch chan<- prometheus.Metric) {
 
 	// when a second request comes, and the first one is already in progress,
 	// ignore/reject the second request
 	mc.Lock()
 	if mc.requestInProgress {
-		mc.rejectRequestCount.Inc()
+		mc.metrics.IncRejectRequestCounter()
 		mc.Unlock()
-		mc.rejectRequestCount.Collect(ch)
+		for _, col := range mc.metrics.ErrorCollectors() {
+			col.Collect(ch)
+		}
 		return
 	}
 
@@ -95,39 +89,42 @@ func (mc *MetricCollector) Collect(ch chan<- prometheus.Metric) {
 	defer mc.setRequestProgressToFalse()
 
 	// get required metric data from etcd
-	bdList, err := mc.getMetricData()
+	blockDevices, err := mc.getMetricData()
 	if err != nil {
 		return
 	}
 
 	// set the metric data into the respective fields
-	mc.setMetricData(bdList)
+	mc.metrics.SetMetrics(blockDevices)
 
 	// collect each metric
-	for _, col := range mc.collectors() {
+	for _, col := range mc.metrics.Collectors() {
 		col.Collect(ch)
 	}
 }
 
 // getMetricData is used to get the metric data from the source
-func (mc *MetricCollector) getMetricData() ([]v1alpha1.BlockDevice, error) {
-	bdlist := &v1alpha1.BlockDeviceList{
+func (mc *StaticMetricCollector) getMetricData() ([]blockdevice.BlockDevice, error) {
+	// list all the BDs from etcd
+	bdList := &apis.BlockDeviceList{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "BlockDevice",
 			APIVersion: "openebs.io/v1alpha1",
 		},
 	}
-	err := mc.Client.List(context.TODO(), nil, bdlist)
+	err := mc.Client.List(context.TODO(), nil, bdList)
 	if err != nil {
 		glog.Error("error in listing BDs", err)
 		return nil, err
 	}
-	return bdlist.Items, nil
-}
 
-// setMetricData sets the metric data into respective fields
-func (mc *MetricCollector) setMetricData(bdList []v1alpha1.BlockDevice) {
-	for _, bd := range bdList {
-		mc.blockDeviceState.WithLabelValues(bd.Name, bd.Spec.Path, bd.Labels[controller.KubernetesHostNameLabel]).Set(getState(bd.Status.State))
+	// convert the BD apis to BlockDevice struct used by NDM internally
+	blockDevices := make([]blockdevice.BlockDevice, 0)
+	for _, bd := range bdList.Items {
+		blockDevice := blockdevice.BlockDevice{}
+		blockDevice.NodeAttributes[blockdevice.HostName] = bd.Labels[controller.KubernetesHostNameLabel]
+		blockDevice.Path = bd.Spec.Path
+		blockDevices = append(blockDevices, blockDevice)
 	}
+	return blockDevices, nil
 }
