56a2deb43eff0361e40d885e6f208341b6b34afd
Kiran Mova <kiran.mova@mayadata.io>
2019-11-29 15:00:32 +0530

    fix(tools): set the default tag to latest for helper pod (#351)
    The default helper pod which can be overridden via the
    CLEANUP_JOB_IMAGE environment variable was set 
    to fixed older version. This can result in default image 
    to have some security vulnerabilities.
    
    As part of the release process, the helper pod images will
    be rebuilt allowing to pull in a image that has fixed
    known security vulnerabilities.
    
    Signed-off-by: kmova <kiran.mova@mayadata.io>

--- pkg/cleaner/config.go
+++ pkg/cleaner/config.go
@@ -29,7 +29,7 @@ const (
 
 var (
 	// defaultCleanUpJobImage is the default job container image
-	defaultCleanUpJobImage = "quay.io/openebs/linux-utils:3.9"
+	defaultCleanUpJobImage = "quay.io/openebs/linux-utils:latest"
 )
 
 // getCleanUpImage gets the image to be used for the cleanup job
