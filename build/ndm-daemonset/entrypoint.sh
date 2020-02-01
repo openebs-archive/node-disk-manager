692804837755a2166353a19c95b7f799eb24c6ea
Akhil Mohan <akhil.mohan@mayadata.io>
2020-02-01 23:33:04 +0530

    feat(script, yaml): customize the location for ndm cores (#362)
    Make the location for saving the NDM cores inline with other
    openebs components by providing the location where core's should
    be saved. 
    
    The default location is /var/openebs/ndm, which can be
    changed by passing the right value in the deployment
    file. 
    
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- build/ndm-daemonset/entrypoint.sh
+++ build/ndm-daemonset/entrypoint.sh
@@ -2,17 +2,28 @@
 
 export GOTRACEBACK=crash
 
+# openebs base directory inside the container
+OPENEBS_BASE_DIR="/var/openebs"
+# ndm base directory inside the container. It will be the ndm
+# directory inside openebs base directory
+NDM_BASE_DIR="${OPENEBS_BASE_DIR}/ndm"
+
 # set ulimit to 0, if the core dump is not enabled
 if [ -z "$ENABLE_COREDUMP" ]; then
   ulimit -c 0
 else
+  # making sure mountpath inside the container is available
+  if ! [ -d "${NDM_BASE_DIR}" ]; then
+    echo "OpenEBS/NDM Base directory not found"
+    exit 1
+  fi
   # set ulimit to unlimited and create a core directory for creating coredump
   echo "[entrypoint.sh] enabling core dump."
   ulimit -c unlimited
-  echo "[entrypoint.sh] creating $SPARSE_FILE_DIR/core if not exists."
-  mkdir -p "$SPARSE_FILE_DIR/core"
-  echo "[entrypoint.sh] changing directory to $SPARSE_FILE_DIR/core"
-  cd "$SPARSE_FILE_DIR/core" || exit
+  echo "[entrypoint.sh] creating ${NDM_BASE_DIR}/core if not exists."
+  mkdir -p "${NDM_BASE_DIR}/core"
+  echo "[entrypoint.sh] changing directory to ${NDM_BASE_DIR}/core"
+  cd "${NDM_BASE_DIR}/core" || exit
 fi
 
 echo "[entrypoint.sh] launching ndm process."
