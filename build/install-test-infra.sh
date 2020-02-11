f4ab1f36ffa0d77331da7da9d08a63b02749510d
Akhil Mohan <akhil.mohan@mayadata.io>
2020-02-11 09:35:44 +0530

    feat(build): enable auto build for arm64 binaries (#371)
    refactor build script and ci scripts to work on multiple platforms.
    Only on amd64, the unit tests and integration tests will be run.
    On all other platforms, the binary and docker images will be built
    and pushed.
    
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

---
+++ build/install-test-infra.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Test infrastructure for running integration tests on NDM.
+# Currently minikube is used to run the integration tests. Since
+# minikube is available only on amd64, integration tests can be run
+# only on that platform
+
+ARCH=$1
+if [ -z "$ARCH" ]; then
+  echo "Test Infra platform not specified. Exiting. "
+  exit 1
+fi
+
+if [ "$ARCH" == "amd64" ]; then
+  curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.0.0/minikube-linux-amd64
+  sudo chmod +x minikube
+  sudo mv minikube /usr/local/bin/
+fi
