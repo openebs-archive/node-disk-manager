648b9536244df6ecfc53e5012431f0736f6d368f
Akhil Mohan <akhil.mohan@mayadata.io>
2020-02-20 14:49:36 +0530

    refact blockdevice controller
    earlier blockdevice controller used to search for the corresponding
    disks, and if the disk were found inactive the blockdevice was also
    made inactive. This has been removed since Disk resource will no
    longer be actively updated with the latest information.
    
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- pkg/controller/blockdevice/blockdevice_controller.go
+++ pkg/controller/blockdevice/blockdevice_controller.go
@@ -18,16 +18,9 @@ package blockdevice
 
 import (
 	"context"
-	"strings"
-
-	"github.com/go-logr/logr"
 	ndm "github.com/openebs/node-disk-manager/cmd/ndm_daemonset/controller"
 	openebsv1alpha1 "github.com/openebs/node-disk-manager/pkg/apis/openebs/v1alpha1"
 	"github.com/openebs/node-disk-manager/pkg/cleaner"
-	"github.com/openebs/node-disk-manager/pkg/udev"
-	"github.com/openebs/node-disk-manager/pkg/util"
-	"k8s.io/apimachinery/pkg/types"
-
 	//corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -131,71 +124,7 @@ func (r *ReconcileBlockDevice) Reconcile(request reconcile.Request) (reconcile.R
 		}
 	}
 
-	err = r.CheckBackingDiskStatusAndUpdateDeviceCR(instance, reqLogger)
-
-	if err != nil {
-		// Error while reading, updating object - requeue the request.
-		return reconcile.Result{}, err
-	}
 	return reconcile.Result{}, nil
-}
-
-// CheckBackingDiskStatusAndUpdateDeviceCR checks the status of the backing DiskCR for a block device CR, and updates
-// the status of Block Device. If the backing Disk of a block-device is marked inactive, then all block devices
-// which use that disk should be marked inactive.
-func (r *ReconcileBlockDevice) CheckBackingDiskStatusAndUpdateDeviceCR(
-	instance *openebsv1alpha1.BlockDevice, reqLogger logr.Logger) error {
-
-	// If the BlockDevice is of type sparse, then we need not check the backing disk
-	// status, As sparse block devices does not have a backing disk.
-	if instance.Spec.Details.DeviceType == ndm.SparseBlockDeviceType {
-		return nil
-	}
-
-	// if NDM managed label
-	if util.CheckFalsy(instance.Labels[ndm.NDMManagedKey]) {
-		return nil
-	}
-
-	// Find the name of diskCR that need to be read from etcd
-	// TODO: This need to be changed, Currently name of disk and blockdevice
-	// are using same string except prefix which would be "blockdevice"/"disk"
-	UUID := strings.TrimPrefix(instance.ObjectMeta.Name, udev.NDMBlockDevicePrefix)
-	Name := udev.NDMDiskPrefix + UUID
-
-	// Fetch the Disk CR
-	diskInstance := &openebsv1alpha1.Disk{}
-	err := r.client.Get(context.TODO(), types.NamespacedName{Name: Name}, diskInstance)
-	if err != nil {
-		reqLogger.Error(err, "Error while getting Disk-CR", "Disk-CR:", Name)
-		if errors.IsNotFound(err) {
-			reqLogger.Error(err, "Disk CR not found", Name)
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
-			dcpyInstance := instance.DeepCopy()
-			dcpyInstance.Status.State = ndm.NDMInactive
-			err := r.client.Update(context.TODO(), dcpyInstance)
-			if err != nil {
-				reqLogger.Error(err, "Error while updating BlockDevice-CR", "BlockDevice-CR", instance.ObjectMeta.Name)
-			}
-			reqLogger.Info("BlockDevice-CR marked Inactive", "BlockDevice:", instance.ObjectMeta.Name)
-			return err
-		}
-		return err
-	}
-
-	if diskInstance.Status.State == ndm.NDMInactive {
-		dcpyInstance := instance.DeepCopy()
-		dcpyInstance.Status.State = ndm.NDMInactive
-		err := r.client.Update(context.TODO(), dcpyInstance)
-		if err != nil {
-			reqLogger.Error(err, "Error while updating BlockDevice-CR", "BlockDevice-CR", instance.ObjectMeta.Name)
-			return err
-		}
-		reqLogger.Info("BlockDevice-CR marked Inactive", "BlockDevice:", instance.ObjectMeta.Name)
-	}
-	return nil
 }
 
 func (r *ReconcileBlockDevice) updateBDStatus(state openebsv1alpha1.DeviceClaimState, instance *openebsv1alpha1.BlockDevice) error {
