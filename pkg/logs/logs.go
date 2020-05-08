8e3ffc9506a5065ed6cbc953781941e9eac43c5c
Akhil Mohan <akhil.mohan@mayadata.io>
2020-05-08 17:16:26 +0530

    refactor(operator): add klog into NDM operator
    - klog has also been added to make logs more readable
    - controller logs has been changed to klog
    - zap logger is still initialized so that controller runtime logs
    are logged into console
    
    Co-authored-by: Mayank <33252549+mynktl@users.noreply.github.com>
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- pkg/logs/logs.go
+++ pkg/logs/logs.go
@@ -33,18 +33,18 @@ func init() {
 	flag.Set("logtostderr", "true")
 }
 
-// GlogWriter serves as a bridge between the standard log package and the glog package.
-type GlogWriter struct{}
+// KlogWriter serves as a bridge between the standard log package and the klog package.
+type KlogWriter struct{}
 
 // Write implements the io.Writer interface.
-func (writer GlogWriter) Write(data []byte) (n int, err error) {
+func (writer KlogWriter) Write(data []byte) (n int, err error) {
 	klog.Info(string(data))
 	return len(data), nil
 }
 
 // InitLogs initializes logs the way we want for kubernetes.
 func InitLogs() {
-	log.SetOutput(GlogWriter{})
+	log.SetOutput(KlogWriter{})
 	log.SetFlags(0)
 	// The default glog flush interval is 30 seconds, which is frighteningly long.
 	go wait.Until(klog.Flush, *logFlushFreq, wait.NeverStop)
@@ -57,5 +57,5 @@ func FlushLogs() {
 
 // NewLogger creates a new log.Logger which sends logs to klog.Info.
 func NewLogger(prefix string) *log.Logger {
-	return log.New(GlogWriter{}, prefix, 0)
+	return log.New(KlogWriter{}, prefix, 0)
 }
