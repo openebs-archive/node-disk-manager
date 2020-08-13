b5aebe29fc56486ce00f4d6e8c6e3b4666fc1e70
Akhil Mohan <akhil.mohan@mayadata.io>
2020-08-13 16:38:40 +0530

    add unit test cases for checking labels applied
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- cmd/ndm_daemonset/probe/customtagprobe_test.go
+++ cmd/ndm_daemonset/probe/customtagprobe_test.go
@@ -1,29 +1,95 @@
 package probe
 
 import (
-	"github.com/openebs/node-disk-manager/blockdevice"
 	"testing"
+
+	"github.com/openebs/node-disk-manager/blockdevice"
+	"github.com/openebs/node-disk-manager/db/kubernetes"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_customTagProbe_FillBlockDeviceDetails(t *testing.T) {
-	type fields struct {
-		tags []tag
-	}
-	type args struct {
-		bd *blockdevice.BlockDevice
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
+func TestCustomTagProbeFillBlockDeviceDetails(t *testing.T) {
+	tests := map[string]struct {
+		bd             *blockdevice.BlockDevice
+		customTags     []tag
+		wantTagLabel   string
+		wantTagLabelOk bool
 	}{
-		// TODO: Add test cases.
+		"no custom tags are provided": {
+			bd: &blockdevice.BlockDevice{
+				Identifier: blockdevice.Identifier{
+					DevPath: "/dev/sda",
+				},
+			},
+			customTags:     nil,
+			wantTagLabel:   "",
+			wantTagLabelOk: false,
+		},
+		"single custom tag using path is present with matching device": {
+			bd: &blockdevice.BlockDevice{
+				Identifier: blockdevice.Identifier{
+					DevPath: "/dev/sda",
+				},
+			},
+			customTags: []tag{
+				{
+					tagType: tagTypePath,
+					regex:   "/dev/sda",
+					label:   "label1",
+				},
+			},
+			wantTagLabel:   "label1",
+			wantTagLabelOk: true,
+		},
+		"single custom tag using path is present without matching device": {
+			bd: &blockdevice.BlockDevice{
+				Identifier: blockdevice.Identifier{
+					DevPath: "/dev/sdb",
+				},
+			},
+			customTags: []tag{
+				{
+					tagType: tagTypePath,
+					regex:   "/dev/sda",
+					label:   "label1",
+				},
+			},
+			wantTagLabel:   "",
+			wantTagLabelOk: false,
+		},
+		"single custom tag with regex using path is present with matching device": {
+			bd: &blockdevice.BlockDevice{
+				Identifier: blockdevice.Identifier{
+					DevPath: "/dev/sdb",
+				},
+			},
+			customTags: []tag{
+				{
+					tagType: tagTypePath,
+					regex:   "/dev/sd[a|b]",
+					label:   "label1",
+				},
+			},
+			wantTagLabel:   "label1",
+			wantTagLabelOk: true,
+		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			bd := tt.bd
 			ctp := &customTagProbe{
-				tags: tt.fields.tags,
+				tags: tt.customTags,
 			}
+			ctp.FillBlockDeviceDetails(bd)
+
+			tagValue, ok := bd.Labels[kubernetes.BlockDeviceTagLabel]
+			assert.Equal(t, tt.wantTagLabelOk, ok)
+
+			if tt.wantTagLabelOk {
+				assert.Equal(t, tt.wantTagLabel, tagValue)
+			}
+
 		})
 	}
 }
