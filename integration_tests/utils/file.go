ae9e47fdbebfa8d96d436746c916714aedfcc808
Atibhi Agrawal <atibhi.a@gmail.com>
2021-05-07 15:01:19 +0530

    refactor(pkg, tests): clean potential file inclusion via variable (#577)
    Signed-off-by: aSquare14 <atibhi.a@gmail.com>

--- integration_tests/utils/file.go
+++ integration_tests/utils/file.go
@@ -20,11 +20,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 // GetYAMLString gets the yaml-string from the given YAML file
 func GetYAMLString(fileName string) (string, error) {
-	fileBytes, err := ioutil.ReadFile(fileName)
+	fileBytes, err := ioutil.ReadFile(filepath.Clean(fileName))
 	if err != nil {
 		return "", err
 	}
