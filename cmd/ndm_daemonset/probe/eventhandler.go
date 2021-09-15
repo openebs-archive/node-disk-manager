1264ccd43adf3f02b201719855721ace95fd0efa
liuminjian <liuminjian@chinatelecom.cn>
2021-09-15 17:04:42 +0800

    skip delete disk partition event when GPTBasedUUID is disable
    Signed-off-by: liuminjian <liuminjian@chinatelecom.cn>

--- cmd/ndm_daemonset/probe/eventhandler.go
+++ cmd/ndm_daemonset/probe/eventhandler.go
@@ -89,7 +89,7 @@ func (pe *ProbeEvent) addBlockDeviceEvent(msg controller.EventMessage) {
 		} else {
 			// if GPTBasedUUID is disabled and the device type is partition,
 			// the event can be skipped.
-			if device.DeviceAttributes.DeviceType == libudevwrapper.UDEV_PARTITION {
+			if device.DeviceAttributes.DeviceType == blockdevice.BlockDeviceTypePartition {
 				klog.Info("GPTBasedUUID disabled. skip creating block device resource for partition.")
 				continue
 			}
@@ -123,7 +123,7 @@ func (pe *ProbeEvent) deleteBlockDeviceEvent(msg controller.EventMessage) {
 		if isGPTBasedUUIDEnabled {
 			_ = pe.deleteBlockDevice(*device, bdAPIList)
 		} else {
-			if device.DeviceAttributes.DeviceType == libudevwrapper.UDEV_PARTITION {
+			if device.DeviceAttributes.DeviceType == blockdevice.BlockDeviceTypePartition {
 				klog.Info("GPTBasedUUID disabled. skip delete block device resource for partition.")
 				isDeactivated = false
 				continue
